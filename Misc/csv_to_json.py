24f94ed82a2ec052ac0a61f77400f81509d3f8d0
Raghavsk24 <senthilkumaraghav@gmail.com>
2025-07-16 18:57:56 -0700

    Update csv_to_json.py

--- Misc/csv_to_json.py
+++ Misc/csv_to_json.py
@@ -1,1 +1,23 @@
+import csv
+import json
+import argparse
 
+def csv_to_json(csv_file_path, json_file_path):
+    data = []
+    with open(csv_file_path, 'r', encoding='utf-8-sig') as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+        for row in csv_reader:
+            data.append(row)
+
+    with open(json_file_path, 'w', encoding='utf-8') as jsonfile:
+        json.dump(data, jsonfile, indent=4)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input_csv", required=True, help="Path to the input CSV file")
+parser.add_argument("--output_json", required=True, help="Path to the output JSON file")
+args = parser.parse_args()
+
+input_csv = args.input_csv
+output_json = args.output_json
+
+csv_to_json(input_csv, output_json)
